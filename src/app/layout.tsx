0fdb7692fda4d60684c353e8ccf97566fe8ced9a
DSAINT <ernestteamit@gmail.com>
2026-06-15 12:59:11 +0200

    Update layout.tsx
    Fix Tailwind CSS loading in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,11 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "DSAINT",
+  description:
+    "Distributed Sovereign AI Infrastructure Nodes Technology",
+};
+
 export default function RootLayout({
   children,
 }: {
